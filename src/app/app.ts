d5455d8c0f10664dd9e5b45a11a63cefa5f7f630
Maiki <132618786+Guerreroangulomiguel@users.noreply.github.com>
2025-10-23 10:46:56 -0700

    Portafolio 20251023

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { AboutMe } from "./about-me/about-me";
+import { Projects } from "./projects/projects";
+import { Contacto } from "./contacto/contacto";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, AboutMe, Projects, Contacto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
